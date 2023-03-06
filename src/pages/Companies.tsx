e6d8bfafa2eaee0c993203bbd38a0714b3011e66
Rodrigo Meneses <rodrigofmeneses97@gmail.com>
2023-03-06 16:27:47 -0300

    feat: :sparkles: Add API requisition to list all companies

--- src/pages/Companies.tsx
+++ src/pages/Companies.tsx
@@ -1,19 +1,30 @@
 import { useNavigate } from 'react-router-dom'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Button, Table } from '../components'
 import style from './styles/Companies.module.scss'
 import routerStyle from './styles/Router.module.scss'
+import { ICompany } from '../types/company'
+import axios from 'axios'
 
 export default function Companies() {
   const navigate = useNavigate()
 
-  const [header, setHeader] = useState(['Name', 'Actions'])
-  const [companies, setEmployees] = useState([
-    {
-      name: 'Maré',
-    },
-    { name: 'LAP' },
-  ])
+  const [header, setHeader] = useState(['ID', 'Name', 'Actions'])
+  const [companies, setCompanies] = useState<ICompany[]>([])
+
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: 'http://localhost:7777/companies',
+    })
+      .then(res => {
+        const resCompanies = res.data
+        setCompanies(resCompanies)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
 
   return (
     <div className={routerStyle.routerContainer}>
@@ -33,7 +44,6 @@ export default function Companies() {
         <Table
           title="Companies"
           checkbox={false}
-          edit={false}
           header={header}
           content={companies}
         ></Table>
